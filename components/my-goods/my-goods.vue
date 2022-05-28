64beb9f0889aa35598086b9a9ed7b6a676a5bb98
qiaoxiaona <2055658234@qq.com>
2022-05-28 13:29:18 +0800

    完成了商品列表页面开发goodslist

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="goods-item">
+		<!-- 左侧图片盒子 -->
+		<view class="goods-item-left">
+			<image class="goods-pic" :src="goods.goods_small_logo || defaultPic"></image>
+		</view>
+		<!-- 右侧图片盒子 -->
+		<view class="goods-item-right">
+			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<view class="goods-price">{{goods.goods_price | toFixed}}元</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			//使用组件的goods_list页面需要使用goods属性
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		data() {
+			return {
+				//给失效的图片地址，添加默认地址。
+				defaultPic:'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+		//使用过滤器处理商品价格
+		filters:{
+			//toFixed为过滤器名字
+			toFixed(num){
+				//Number是将外界传递进来的值（有可能是字符串格式的值）转化为数字型，然后toFixed（）保留两位小数，输出
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+	display: flex;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.goods-item-left{
+		margin-right: 5px;
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+			
+		}
+	}
+	.goods-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		
+		.goods-name{
+			font-size: 13px;
+			
+		}
+		.goods-info-box{
+			.goods-price{
+				color: #C00000;
+				font-size: 16px
+			}
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
-		goods_list
+		<view class="goods-list">
+			<!-- block不支持绑定点击事件,需要改为view
+			<block v-for="(goods,i) in goodsList" :key="i">
+				<my-goods :goods="goods"></my-goods>
+			</block> -->
+			
+			<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,9 +17,89 @@
 	export default {
 		data() {
 			return {
-				
+				 // 请求参数对象
+				        queryObj: {
+							// 首页楼层，搜索历史（传递query参数),商品三级详情(传递cid)
+				          query: '',
+				          cid: '',
+						  //默认页码，每个页面条数
+				          pagenum: 1,
+				          pagesize: 10
+				        },
+						//商品列表的数据
+						goodsList:[],
+						//商品总数量，用来实现分页
+						total:0,
+						//节流阀
+						isloading:false
+		
 			};
-		}
+		},
+		//options接收页面跳转传递的参数，有三种传递参数方式，首页楼层，搜索历史（传递query参数),商品三级详情(传递cid)
+		onLoad(options){
+			//把页面请求的参数传递给data,考虑有可能参数为空的可能
+			//console.log(options);
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			//console.log(this.queryObj);
+			
+			//获取商品列表数据
+			this.getGoodsList()
+		},
+		methods:{
+			//获取商品列表数据
+			//这里的cb指的是触发下拉刷新效果时传过来的回调函数。由于 getGoodsList()多处调用，不一定每次传cb，因此需要在调用cb前判断
+			async getGoodsList(cb){
+				//打开节流阀
+				this.isloading=true
+				
+				//返回值是一个对象，可以结构赋值为data属性,重命名为res
+				const {data:res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				
+				//关闭节流阀
+				this.isloading=false
+				
+				//用短路运算符来判断，如果cb值传进来了，就调用，如果没有就不调用后面的
+				cb && cb()
+				
+				if(res.meta.status !==200) return uni.$showMsg()
+				//console.log(res);
+				//原来的只是返回第一页数据，在触发上拉事件后，需要增加新页码的数据
+				// this.goodslist = res.message.goods
+				//新旧数据拼接
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.total = res.message.total
+			},
+			//点击商品item项，跳转到详情页面。
+			gotoDetail(goods){
+				uni.navigateTo({
+					 url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		},
+		//触动下拉触底效果
+		onReachBottom(){
+			//判断是否还有下一页数据
+			if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total)return uni.$showMsg("数据加载完毕")
+			
+			//判断节流阀，当节流阀为true时，代表正在请求数据，阻止数据多次请求
+			if(this.isloading)return
+			//让触发触底时，页码值自增+1
+			this.queryObj.pagenum ++
+			this.getGoodsList()
+		},
+		//触动下拉刷新
+		onPullDownRefresh(){
+			//重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+				  
+			//重新发起数据请求。用回调函数关闭下拉刷新效果。
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
+		},
+		
 	}
 </script>
 
